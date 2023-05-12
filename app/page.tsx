e3714ae1ae2e7fe4554c5ff5d10fab795640ce14
Minu Kim <minumoseskim@gmail.com>
2023-05-11 22:56:55 -0400

    hello world

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+import { Redis } from '@upstash/redis';
+
+const redis = Redis.fromEnv();
+
+export const revalidate = 0; // disable cache
+
+export default async function Home() {
+  const member = await redis.srandmember<string>('nextjs13');
+
+  return (
+    <div>
+      <main>
+        <h1>Welcome {member}</h1>
+      </main>
+      <p>You have been randomly chosen by the redis algorithm.</p>
+    </div>
+  );
 }
